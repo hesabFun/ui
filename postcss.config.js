279e06359437a804b3314a018221a684f9f3fb31
ErFUN KH <erfun.kh@gmail.com>
2020-04-09 15:45:00 +0300

    added: components

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ const tailwindcss = require("tailwindcss");
 
 // only needed if you want to purge
 const purgeCss = require("@fullhuman/postcss-purgecss")({
-    content: ["./src/**/*.svelte", "./src/**/*.html", "./static/styles/*", "./static/styles/*/*"],
+    content: ["./src/**/*.svelte", "./src/**/*.html"],
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
 
