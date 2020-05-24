5d9644937d34778fd2866f6b88914737a73d17d3
ErFUN KH <erfun.kh@gmail.com>
2020-05-24 11:30:53 +0300

    added pages

---
+++ src/pages/forgot-password.svelte
@@ -0,0 +1,26 @@
+<div class="grid grid-flow-col grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1">
+    <div class="w-full h-screen grid grid-rows-2">
+        <div class="m-12">
+            <a href="../" class="mt-4 p-4 bg-white-text font-light text-1xl text-center tracking-wide re"><span class="">&#8592</span> Back to home</a>
+            <h1 class="mt-8 text-2xl sm:text-4xl tracking-tight font-bold">Reset your<br> password here</h1>
+            <div class="max-w-6xl">
+                <form>
+                    <label class="hidden" for="email">Email:</label><br>
+                    <input class="p-3 bg-white-text border border-greyish font-medium w-64" type="text" id="email" name="email" placeholder="Email"><br>
+                    <input class="mt-5 p-3 bg-butterscotch text-1xl tracking-tight w-64 cursor-pointer" type="submit" value="Reset password">
+                </form>
+                <hr class="mt-8 w-64">
+                <p class="w-64 -mt-3 mb-4 text-center text-gray-700 text-black"><span class="bg-gray-100 p-2">OR</span></p>
+                <a href="../" class="mt-4 bg-white border border-greyish w-64 p-3 self-center text-1xl tracking-tight font-bold relative flex flex"><img class="justify-center w-6 mr-4 ml-3" src="/images/icons/btn_google_icon.svg" alt="hesabfun logo">Sign up with Google</a>
+                <p class="mt-3 w-64 text-sm tracking-tight font-bold text-greyish">Return to <a href="/signin" class="font-bold text-greyish-brown hover:text-butterscotch">Sign in</a></p>
+            </div>
+        </div>
+    </div>
+    <div class="w-full h-screen bg-greyish-brown text-white-text relative">
+        <div class="m-12 flex flex-col">
+            <span class="mt-14 text-6xl w-full text-center">❞</span>
+            <h2 class="mt-8 text-4xl sm:text-5xl w-full text-center">Creativity is great but not in accounting.</h2>
+            <h3 class="mt-16 text-1xl sm:text-2xl w-full text-center">Charles Scott</h3>
+        </div>
+    </div>
+</div>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,29 +1,31 @@
-<script>
-    export let name = 'erfun';
-</script>
-<main>
-    <h1>Hello {name}!</h1>
-    <p>Visit the <a href="about">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<svelte:head>
+	<title>Hesabfun - A base API accounting system</title>
+</svelte:head>
 
-<style>
-    main {
-        text-align: center;
-        padding: 1em;
-        max-width: 240px;
-        margin: 0 auto;
-    }
-
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
-    }
-</style>
+<div class="grid grid-flow-col grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 bg-greyish-brown">
+	<div class="bg-white w-full h-screen grid grid-rows-2 lg:col-start-2">
+		<div class="flex flex-col">
+			<img class="justify-center mt-12" src="hesabfun-logo.svg" alt="hesabfun logo">
+			<p class="font-bold text-center text-6xl text-greyish-brown mt-2">Hesabfun</p>
+			<p class="font-mono text-center tracking-tight font-book text-greyish -mt-3">open source financial platform</p>
+			<a href="signup" class="mt-12 bg-butterscotch w-64 p-4 self-center text-1xl text-center tracking-tight">Create your free account</a>
+			<a href="../" class="mt-4 bg-white border border-greyish w-64 p-3 self-center text-1xl tracking-tight font-bold relative flex flex"><img class="justify-center w-6 mr-4 ml-3" src="images/icons/btn_google_icon.svg" alt="hesabfun logo">Sign up with Google</a>
+			<p class="mt-6 tracking-tight font-bold text-greyish text-center">Already have an account? <a class="hover:text-butterscotch" href="signin">Sign in</a></p>
+		</div>
+		<div class="self-end mb-4 flex justify-center text-greyish tracking-tight font-bold text-brown-grey">
+			<a href="https://www.linkedin.com/company/hesabfun" class="hover:text-butterscotch" target="blank">Linkedin</a>
+			<a href="https://twitter.com/hesabfun" class="ml-8 hover:text-butterscotch" target="blank">Twitter</a>
+			<a href="https://github.com/hesabfun" class="ml-8 hover:text-butterscotch" target="blank">Github</a>
+		</div>
+	</div>
+	<div class="w-full h-screen text-gray-300 lg:col-start-1">
+		<div class="m-6 sm:m-12 text-white-text sm:text-justify">
+			<h1 class="text-2xl">What is Hesabfun?</h1>
+			<p class="mt-2">Hesabfun is a base API accounting system, it's open source and free. you can use HesabFun site for free or run it under 5 minutes by docker on your company's server.</p>
+			<h1 class="text-2xl mt-6">Why do we need a base API accounting system?</h1>
+			<p class="mt-2">Probably you as a developer don’t know much about accounting, tax, value-added, etc.</p>
+			<p class="mt-2">But as far as we want salary from companies that we work for, companies should have income and us developers write financial system platforms, we have a lot of problems in this part, for example when your CTO comes in and says that accounting needs that kind of sale’s report and you had never designed it to get reports easily, or worth than that is when you have to export the reports, for example in the format of Excel so traditional accounting systems can be imported once a month.</p>
+			<p class="mt-2">or there would be another kind of problem when you want to create something like CRM or E-commerce for a company that has done all things traditionally, inventory report or the last list of customers is like a nightmare. these systems don’t have any API, just designed for accountants to work with and get the daily reports.</p>
+		</div>
+	</div>
+</div>
